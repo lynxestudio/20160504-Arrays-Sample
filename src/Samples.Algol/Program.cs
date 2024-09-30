fc2299a62becad9751800a6a21735d343e7e7b47
Martin A. Ortega <lynxestudio@outlook.com>
2024-09-29 22:36:40 -0600

    Adds source code to the project

---
+++ src/Samples.Algol/Program.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Samples.Algol
+{
+ class Program
+ {
+  public static void Main(string[] args)
+  {
+   int[] numbers = { 10, 84,654,42,66, 87, 3,11, 60,666};
+   Console.WriteLine ("Disordered array");
+   Print (numbers);
+   for(int i = 0; i < numbers.Length;i++)
+   {
+    for(int j = i + 1;j < numbers.Length;j++)
+    {
+     if(numbers[j] < numbers[i])
+     {
+      int temp = numbers[i];
+      numbers[i] = numbers[j];
+      numbers[j] = temp;
+     }
+    }
+   }
+   Console.WriteLine ("Ordered array");
+   Print (numbers);
+   Console.WriteLine("Press enter to continue...");
+   Console.ReadLine();
+  }
+
+  static void Print(int[] numbers)
+  {
+   Console.WriteLine();
+   foreach(var item in numbers)
+   {
+    Console.Write(" [{0}]",item);
+   }
+   Console.WriteLine (Environment.NewLine);
+  }
+ }
+}
